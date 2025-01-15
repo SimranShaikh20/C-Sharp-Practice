f168d6a633c7af36ca67fa9ddb761183b5f849ee
Simran Shaikh <simmoshaikh20.2003@gmail.com>
2025-01-15 11:22:16 +0530

    commit

---
+++ Product Inventory System/ProductMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Inventory_System
+{
+    public class ProductMethods
+    {
+        public class AddItem
+        {
+            public void GetData()
+            {
+                Console.Write("Enter item name to add : ");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Enter price of{name} : ");
+                int price=int.Parse( Console.ReadLine() );
+                Console.WriteLine("Item Added");
+                Console.WriteLine("---------------------");
+            }
+        }
+        public class UpdateItem:AddItem
+        {
+            
+        }
+    }
+}
---
+++ Product Inventory System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Product_Inventory_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
