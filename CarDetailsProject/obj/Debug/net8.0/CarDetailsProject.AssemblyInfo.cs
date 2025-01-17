184b426cf1c944f1437b4151bd2e20083ac86c26
Simran Shaikh <simmoshaikh20.2003@gmail.com>
2025-01-17 19:47:22 +0530

    commit

--- CarDetailsProject/obj/Debug/net8.0/CarDetailsProject.AssemblyInfo.cs
+++ CarDetailsProject/obj/Debug/net8.0/CarDetailsProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarDetailsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70fbed293e19f77a10f76abaa73cdb383512724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f5b619eef84a0d96f247e23077bd575b96a1d8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarDetailsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarDetailsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
